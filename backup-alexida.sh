40d2bc6b211d3bf92867def4bb3f4903b9002d34
Wil van Antwerpen <43706927+wilva@users.noreply.github.com>
2018-09-30 00:59:43 +0200

    Demo script for backup running VM

---
+++ backup-alexida.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# Imports the entire alexida machine as a ready to run VM from our local storage.
+# The import is from a LIVE VM, folder name = vmx name = first disk name
+# Old imports WILL be removed and overwritten! 
+
+strVMname="Alexida"
+strLUN="/vmfs/volumes/storage1"
+strIMPORT="/vmfs/volumes/backups"
+
+rm -rf ${strIMPORT}/${strVMname}
+
+# load vmfunctions
+. /vmfs/volumes/bup/scripts/vmfunctions.sh
+
+rm -rf ${strIMPORT}/${strVMname}
+
+setupBackupLocation "${strVMname}" "${strLUN}" "${strIMPORT}"
+
+createSnapshot "${strVMX}" ${VMID}
+
+cloneVMDK "${strLUN}/${strVMname}/${strVMname}.vmdk" "${strIMPORT}/${strVMname}/${strVMname}.vmdk" "2gbsparse"
+
+commitSnapshot "${strVMX}" ${VMID}
+
+finishBackup "${strVMname}" "${strLUN}"
+
